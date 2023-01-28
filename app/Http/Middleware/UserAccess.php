c508dd9860808bbf1cfb99f20c3a475f490f880b
marcellajinx <cellakomunita@gmail.com>
2023-01-28 10:51:41 +0700

    add 403 err page

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -20,7 +20,7 @@ class UserAccess
             return $next($request);
         }
            
-        return response()->json(['You do not have permission to access for this page.']);
-        /* return response()->view('errors.check-permission'); */
+        // return response()->json(['You do not have permission to access for this page.']);
+        return abort(403);
     }
 }
