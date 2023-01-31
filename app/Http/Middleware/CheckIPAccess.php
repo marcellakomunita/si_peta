abe607b4a3d05737ca8f7bff0fa1fa97e5ee43a8
marcellajinx <cellakomunita@gmail.com>
2023-02-01 06:30:06 +0700

    sync ip

--- app/Http/Middleware/CheckIPAccess.php
+++ app/Http/Middleware/CheckIPAccess.php
@@ -16,7 +16,7 @@ class CheckIPAccess
      */
     public function handle(Request $request, Closure $next)
     {
-        $libraryIpRange = '127.0.0.1/182.2.71.79'; // Example IP range
+        $libraryIpRange = env('ALL_IP'); // Example IP range
         if (!in_array($_SERVER['REMOTE_ADDR'], explode('/', $libraryIpRange)) && $request->path() !== 'unauthorized') {
             // dd($_SERVER['REMOTE_ADDR']);
             return abort(401);
