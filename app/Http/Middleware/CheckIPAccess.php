d0c8f0165091acbbd5cf0b0fc4802507e640f61f
marcellajinx <cellakomunita@gmail.com>
2023-02-09 05:46:37 +0700

    fix bug in ip access

--- app/Http/Middleware/CheckIPAccess.php
+++ app/Http/Middleware/CheckIPAccess.php
@@ -14,9 +14,18 @@ class CheckIPAccess
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
+    protected function isValidIp($ip) {
+        return preg_match("/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/", $ip) && count(array_filter(explode('.', $ip), function($v) { return ($v >= 0 && $v <= 255); })) === 4;
+    }
     public function handle(Request $request, Closure $next)
     {
-        $ip = $request->ip();
+        $ip = $_SERVER['HTTP_X_ENVOY_EXTERNAL_ADDRESS'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '';
+        $ip = explode(',', $ip)[0];
+        $ip = trim($ip);
+        
+        if(!$this->isValidIp($ip)) {
+            return abort('401');
+        }
 
         $allowed_ranges = [
             ['172.24.151.11', '172.24.151.250'],
@@ -24,7 +33,6 @@ class CheckIPAccess
         ];
         foreach ($allowed_ranges as $range) {
             if (ip2long($ip) >= ip2long($range[0]) && ip2long($ip) <= ip2long($range[1])) {
-                dd('x', $ip, $range);
                 return $next($request);
             }
         }
@@ -34,11 +42,9 @@ class CheckIPAccess
         ];
         foreach ($allowed_ips as $allowed) {
             if (ip2long($ip) == ip2long($allowed)) {
-                dd('y', $ip, $allowed);
                 return $next($request);
             }
         }
-        dd('z', $ip);
 
         return abort('401');
     }
